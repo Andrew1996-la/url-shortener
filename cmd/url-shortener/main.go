bcfad7d8e6f4fda62f593bd5e04e7109abc46381
Andrew <asoborev@mail.ru>
2026-03-07 00:14:26 +0300

    feat: инициализация логгера slog

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,10 +1,43 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/Andrew1996-la/url-shortenerr/internal/config"
+)
+
+const (
+	localEnv = "local"
+	devEnv   = "dev"
+	prodEnv  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
+	logger := setupLogger(cfg.Env)
 
+	logger.Info("starting server", slog.String("env", cfg.Env))
+}
+
+// Конфигурация логгера пода разные окружения
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case localEnv:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case devEnv:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case prodEnv:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
